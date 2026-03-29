88da988630622599567528f593345e12ac1d7c76
Condepro20 <carlosjconde593@gmail.com>
2026-03-29 12:49:42 -0600

    Primer commit para iniciar la app

---
+++ huespedes-msv/src/main/java/com/example/huespedes/HuespedesMsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.huespedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuespedesMsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuespedesMsvApplication.class, args);
+	}
+
+}
